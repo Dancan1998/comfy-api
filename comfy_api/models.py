e9a42b3afd4da73e48375e73b08e5a00797e3d13
Dancan Chibole <dancankingstar@gmail.com>
2021-02-21 14:42:45 -0800

    initalized staticfiles & comfy_api models

--- comfy_api/models.py
+++ comfy_api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=150)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(max_length=250)
+
+
+class Image(models.Model):
+    name = models.CharField(max_length=150, blank=True, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='media')
+    default = models.BooleanField(default=False)
+
+
+class Color(models.Model):
+    name = models.CharField(max_length=150, blank=True, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
