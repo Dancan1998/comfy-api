c2ecc24e36298fb66b39bd3ec1dd7a982161d059
Dancan Chibole <dancankingstar@gmail.com>
2021-02-21 14:44:42 -0800

    added models to admin

--- comfy_api/admin.py
+++ comfy_api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Product, Color, Image
 
-# Register your models here.
+admin.site.register(Product)
+admin.site.register(Image)
+admin.site.register(Color)
