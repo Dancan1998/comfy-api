b9264471b76500f3e9d247bd5f56b20758efeffc
Dancan Chibole <dancankingstar@gmail.com>
2021-02-21 19:40:11 -0800

    detail view

--- comfyapi/views.py
+++ comfyapi/views.py
@@ -7,3 +7,8 @@ from .models import Product
 class ProductListView(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+
+class ProductDetailView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
