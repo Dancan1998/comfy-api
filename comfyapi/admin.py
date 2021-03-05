a0d5d3bad897d13d8d6955552b45cbc835e47859
Dancan Chibole <dancankingstar@gmail.com>
2021-03-05 14:48:50 -0800

    created order & orderitem models

--- comfyapi/admin.py
+++ comfyapi/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Product, Image, Color, ShippingProfile
+from .models import Product, Image, Color, ShippingProfile, Order, OrderItem
 
 admin.site.register(Product)
 admin.site.register(Image)
 admin.site.register(Color)
 admin.site.register(ShippingProfile)
+admin.site.register(OrderItem)
+admin.site.register(Order)
